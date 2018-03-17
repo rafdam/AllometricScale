3f3f8848f0418ccd48ba035af3d21566c182d7f7
rafdam <rafjankowski1996@gmail.com>
2018-03-17 16:28:36 +0100

    Ready3DModel

--- AllometricScaleInDistributionNetworks/src/GUI/TestConsolePrint.java
+++ AllometricScaleInDistributionNetworks/src/GUI/TestConsolePrint.java
@@ -8,26 +8,20 @@ public class TestConsolePrint {
 
 	public TestConsolePrint() {
 	}
-
+         
 	public static void main(String[] args) {
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
 		        if ("Nimbus".equals(info.getName())) {
 		            UIManager.setLookAndFeel(info.getClassName());
-		            //break;
 		        }
 		    }
 		} catch (Exception e) {
 		    System.out.println("kek");
 		}
-		BasicFrame framcia = new BasicFrame();
-		framcia.setVisible(true);
-		IndexCalculus index = new IndexCalculus(8,1,5,5);
-		//index.calc();
-		//framcia.getPane().getCountTab().getChart().addPointsToChart(1, 1);
-		//framcia.getPane().getCountTab().getChart().addPointsToChart(2, 2);
-		//framcia.getPane().getCountTab().getChart().repaint();
-		//framcia.getPane().getCountTab().getChart().revalidate();
+		BasicFrame frame = new BasicFrame();
+		frame.setVisible(true);
+		IndexCalculus index = new IndexCalculus(10,1,15,5);
 	}
 
 }
